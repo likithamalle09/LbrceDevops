2162e731fd88e41b84849d150b07ea47db5d8415
AdmiN <AdmiN@ITWS0039>
2024-08-21 15:09:24 +0530

    commit

---
+++ src/lbrcedevops/Signin.java
@@ -0,0 +1,34 @@
+package lbrcedevops;
+
+import java.util.Scanner;
+
+public class Signin {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+        System.out.print("Welcome to Github");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username");
+
+        String uname=sc.next();
+
+        System.out.print("Enter password");
+
+        String pwd=sc.next();
+
+        System.out.print("Your username"+uname);
+
+        System.out.print("Your password"+pwd);
+
+	}
+
+
+
+}
+
